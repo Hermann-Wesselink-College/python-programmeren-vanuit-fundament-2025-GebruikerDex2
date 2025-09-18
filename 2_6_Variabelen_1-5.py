45114479d0843e918c76dbce0ce863116eb3102a
GebruikerDex2 <dexbriet2009@gmail.com>
2025-09-18 09:49:13 +0200

    Update 2_6_Variabelen_1-5.py

--- 2_6_Variabelen_1-5.py
+++ 2_6_Variabelen_1-5.py
@@ -1,1 +1,17 @@
 # opdracht 1 tpt en met 5 van paragraaf 2.6
+	postcode = "3343 PL"
+print(postcode.lower())
+
+woonplaats = "Hendrik-Ido-Ambacht"
+print(woonplaats.upper())
+
+	verzendmethode = "PostNL"
+betaalmethode = "iDeal"
+print(verzendmethode.lower())
+print(betaalmethode.upper())
+
+	postcode  = input("Wat is je postcode?")
+print(postcode.upper())
+
+	plaats = "Gasselternijveenschemond"
+print(len(plaats))
