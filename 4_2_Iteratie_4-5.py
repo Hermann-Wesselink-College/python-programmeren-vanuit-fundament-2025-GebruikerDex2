a317fedac400712ef79a320c1b5d97e85fa50175
GebruikerDex2 <dexbriet2009@gmail.com>
2025-10-13 10:38:36 +0200

    Update 4_2_Iteratie_4-5.py

--- 4_2_Iteratie_4-5.py
+++ 4_2_Iteratie_4-5.py
@@ -1,1 +1,9 @@
 #Opdrachten 4 en 5 van paragraaf 4.2 
+	jaar = 0
+saldo = 100
+while saldo < 400: 
+    saldo = saldo * 1.1
+    jaar = jaar + 1
+print("Jaar is: " + str(jaar))
+print("Saldo is: " + str(saldo))
+ 
