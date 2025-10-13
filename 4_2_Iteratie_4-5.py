c5e119ad68aeaf789751e34ffc7a9314c7234475
GebruikerDex2 <dexbriet2009@gmail.com>
2025-10-13 11:03:51 +0200

    Update 4_2_Iteratie_4-5.py

--- 4_2_Iteratie_4-5.py
+++ 4_2_Iteratie_4-5.py
@@ -7,3 +7,19 @@ while saldo < 400:
 print("Jaar is: " + str(jaar))
 print("Saldo is: " + str(saldo))
  
+saldo = 100000
+opname = 5000
+jaar = 0
+ 
+while ((saldo * 1.04) - opname) > 0:
+  saldo = saldo * 1.04
+  saldo = saldo - opname
+  jaar = jaar + 1
+  
+  if jaar >= 100:
+    break
+  
+if jaar >= 100:
+  print("Dat kan je hele leven!")
+else:
+  print("Dit kan " + str(jaar) + " jaar")
