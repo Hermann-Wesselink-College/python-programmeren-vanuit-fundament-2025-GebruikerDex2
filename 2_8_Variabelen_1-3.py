0e1d94625418b53faf93787cb269527cd383d8ce
GebruikerDex2 <dexbriet2009@gmail.com>
2025-10-16 11:53:49 +0200

    Update 2_8_Variabelen_1-3.py

--- 2_8_Variabelen_1-3.py
+++ 2_8_Variabelen_1-3.py
@@ -1,1 +1,18 @@
 # opdrachten 1 tot en met 3 van pragraaf 2.8
+
+# Bereken de kosten van een taxirit 
+
+Starttarief: €4,00
+Prijs per kilometer: €1,50
+Prijs per minuut wachttijd (bij stilstand of file): €0,25
+Integer aantalKilometers
+integer wachttijd
+
+output: ''Hoeveel kilometers heeft u gereden?''
+input: aantalKilometers
+output: aantalKilometrs * 1.50
+
+output:''Hoelang heeft u in de file gestaan?"
+input:  wachttijd 
+output: prijs per minuut wachttijd (bij stilstand of file): €0,25 * wachttijd
+
